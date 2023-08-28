3354eb1076c9cb11c927d5b4fdc277c6f8b8cd08
AttiqueMinhas <03-134192-023@student.bahria.edu.pk>
2023-08-28 22:22:20 +0500

    redo the states_services file like asif taj

--- lib/Services/states_services.dart
+++ lib/Services/states_services.dart
@@ -19,17 +19,12 @@ class SplashServices {
 
   Future<List<dynamic>> countriesListApi() async {
     var data;
-    try {
-      final response = await http.get(Uri.parse(AppURL.countriesList));
-      if (response.statusCode == 200) {
-        data = jsonDecode(response.body);
-        return data;
-      } else {
-        return data;
-        //throw Exception('Error');
-      }
-    } catch (e) {
-      throw (e.toString());
+    final response = await http.get(Uri.parse(AppURL.countriesList));
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception('Error');
     }
   }
 }
